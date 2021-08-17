f9ff3f372987ea4f60f1e848ed3dbf8602fdda81
IvaniaPires <ivania.n.pires@gmail.com>
2021-08-17 15:44:57 +0200

    up and down basics

--- 2048.js
+++ 2048.js
@@ -2,10 +2,17 @@ let cells = document.querySelectorAll(".cell");
 let game = document.querySelector("body");
 let randomValues = [2,4];
 let board = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0];
+let randomPosition;
 
-/*for (let i = 0; i < cells.length; i++){
-	cells[i].innerHTML = i;
-}*/
+function arraysAreIdentical(arr1, arr2){
+    if (arr1.length !== arr2.length) return false;
+    for (var i = 0, len = arr1.length; i < len; i++){
+        if (arr1[i] !== arr2[i]){
+            return false;
+        }
+    }
+    return true; 
+}
 
 
 function hasardPosition(){
@@ -15,7 +22,8 @@ function hasardPosition(){
 			aux.push(i);
 		}			
 	}
-	return aux[Math.floor(Math.random() * aux.length)];
+	randomPosition = aux[Math.floor(Math.random() * aux.length)];
+	return randomPosition;
 }
 
 function hasardValue(){
@@ -26,20 +34,176 @@ function fillBoard(){
 	for (let i = 0; i < cells.length; i++){
 		if(board[i]!==0){
 			cells[i].innerHTML = board[i];
+		} else{
+			cells[i].innerHTML = "";
 		}	
 	}
 }
 
-function newPlay(){
-	board[hasardPosition()] = hasardValue();
-	fillBoard();
+function newPlay(){	
+	board[hasardPosition()] = hasardValue();	
 }
 
-board[hasardPosition()] = hasardValue();
+function up (column){
+	for(let i = 0; i<3; i++){
+		if(column[i]===0){
+			for (let j = i+1; j<4; j++){
+				if(column[j]!==0){
+					column[i]=column[j];
+					column[j]=0;
+					j=4;
+				}
+			}
+		}
+	}
+	for(let i = 0; i<3; i++){
+		if(column[i]===column[i+1]){
+			column[i] += column[i+1];
+			column[i+1]=0;
+		}
+	}
+	for(let i = 0; i<3; i++){
+		if(column[i]===0){
+			for (let j = i+1; j<4; j++){
+				if(column[j]!==0){
+					column[i]=column[j];
+					column[j]=0;
+					j=4;
+				}
+			}
+		}
+	}
+}
+
+function down(column){
+	for(let i = 3; i>0; i--){
+		if(column[i] === 0){
+			for(let j = i-1 ; j>=0 ; j--){
+				if(column[j]!==0){
+					column[i] = column[j];
+					column[j] = 0;
+					j=-1;
+				}
+			}
+		}
+	}
+	for(let i = 3; i>0; i--){
+		if(column[i]===column[i-1]){
+			column[i] += column[i-1];
+			column[i-1]=0;
+		}
+	}
+	for(let i = 3; i>0; i--){
+		if(column[i] === 0){
+			for(let j = i-1 ; j>=0 ; j--){
+				if(column[j]!==0){
+					column[i] = column[j];
+					column[j] = 0;
+					j=-1;
+				}
+			}
+		}
+	}
+}
+
+function moveRight(row){
+	
+}
+
 newPlay();
+newPlay();
+fillBoard();
 
 game.addEventListener( "keyup", evt=> {
+	let col1 =[];
+	let col2 =[];
+	let col3 =[];
+	let col4 =[];
+	let row1 =[];
+	let row2 =[];
+	let row3 =[];
+	let row4 =[];
+	let copyBoard = [];	
 	if(evt.key === "ArrowUp"){
-		newPlay();	
+		for(let i = 0; i<16; i++){
+			if(i % 4 === 0 ){
+				col1.push(board[i]);
+			}else if(i % 4 === 1 ){
+				col2.push(board[i]);
+			}else if(i % 4 === 2 ){
+				col3.push(board[i]);
+			}else{
+				col4.push(board[i]);
+			}  
+		}		
+		up(col1);
+		up(col2);
+		up(col3);
+		up(col4);
+
+		for(let i = 0; i<4; i++){
+			copyBoard.push(col1[i]);
+			copyBoard.push(col2[i]);
+			copyBoard.push(col3[i]);
+			copyBoard.push(col4[i]);
+		}		
+		if(!arraysAreIdentical(board, copyBoard)){			
+			for(let i = 0; i<16; i++){
+				board[i]=copyBoard[i];
+			}
+			newPlay();
+			fillBoard();			
+		}	
+		
+
 	};
+	if(evt.key === "ArrowDown"){
+		for(let i = 0; i<16; i++){
+			if(i % 4 === 0 ){
+				col1.push(board[i]);
+			}else if(i % 4 === 1 ){
+				col2.push(board[i]);
+			}else if(i % 4 === 2 ){
+				col3.push(board[i]);
+			}else{
+				col4.push(board[i]);
+			}  
+		}		
+		down(col1);
+		down(col2);
+		down(col3);
+		down(col4);
+
+		for(let i = 0; i<4; i++){
+			copyBoard.push(col1[i]);
+			copyBoard.push(col2[i]);
+			copyBoard.push(col3[i]);
+			copyBoard.push(col4[i]);
+		}		
+		if(!arraysAreIdentical(board, copyBoard)){			
+			for(let i = 0; i<16; i++){
+				board[i]=copyBoard[i];
+			}
+			newPlay();
+			fillBoard();			
+		}
+	};
+	if(evt.key === "ArrowRight"){
+		for(let i = 0; i < 16; i++){
+			if(i < 4 ){
+				row1.push(board[i]);
+			}else if(i < 8 ){
+				row2.push(board[i]);
+			}else if(i < 12 ){
+				row3.push(board[i]);
+			}else{
+				row4.push(board[i]);
+			}  
+		}		
+		moveRight(row1);
+		moveRight(row2);
+		moveRight(row3);
+		moveRight(row4);
+	}
+
 });
